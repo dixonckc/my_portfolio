f3b311feb05a1bc9776089329c8234571f360f02
dixoncck <dixon@DixondeAir.lan>
2023-10-02 01:07:28 -0400

    Description of what you did

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ app.get('/contact', (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
